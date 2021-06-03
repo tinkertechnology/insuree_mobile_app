a27c2bd54a66d0d48f512ecdf930fca7981b94b9
Killua@Sunil <Killua@sunil.com>
2021-06-03 21:02:51 +0545

    merged

--- lib/homescreen/homepage.dart
+++ lib/homescreen/homepage.dart
@@ -81,7 +81,7 @@ class _HomeScreenState extends State<HomeScreen> {
 														Navigator.push(
 															context,
 															MaterialPageRoute(
-																builder: (context) => ProfilePageView(),
+																builder: (context) => ClaimedItemServicesPage(claimid: int.parse(claims.id)),
 															),
 														);
 													},
