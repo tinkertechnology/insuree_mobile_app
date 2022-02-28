e715a6fc6b1858c2bf6cc1462eaf0209508cde60
zoldyck <abc@gmail.com>
2022-02-28 09:12:50 +0545

    common

--- lib/common/env.dart
+++ lib/common/env.dart
@@ -39,15 +39,6 @@ setAuth(AuthBlock ab){
 	auth =ab;
 }
 
-getAuth(){
-	if(auth!=null){
-		if(auth.isLoggedIn==false){
-			return null;
-		}
-	}
-	return auth;
-}
-
 
 String CHFID;
 
@@ -72,43 +63,14 @@ getFirebaseToken(){
 }
 
 
-SaveUpdateFirebaseToken() async {
-	var url = API_BASE_URL+'/api/SaveUpdateFirebaseToken/';
-	http.Response response = await http.post(
-		Uri.parse(url),
-		headers: <String, String>{
-			'Content-Type': 'application/json; charset=UTF-8',
-			'Authorization': 'JWT ' + getAuthToken(null),
-			//'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozLCJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInVzZXJuYW1lIjoicm9vdCIsImV4cCI6MTU4ODkwNjY4OSwib3JpZ19pYXQiOjE1ODg4NzY2ODl9.pmXxw9p10Jzfkx0auWglaarDGG0fnEqgC'//
-		},
-		body: jsonEncode(<String, String>{
-			'firebase_token': getFirebaseToken(),
-		}),
-	);
-	print(response.body);
-	if (response.statusCode == 200) {
-	}
-
-}
 
 
 
-bool hasAuthUserObject(){
-	var _auth = getAuth();
-	if(_auth!=null){
-		if(auth.user!=null){
-			return true;
-		}
-	}
-	return false;
-}
 
 
 String API_BASE_LOCAL_URL =  "http://imistest.hib.gov.np/api/graphql"; //"http://10.0.2.2/api/graphql";//"http://imistest.hib.gov.np/api/graphql";//"https://oi.tinker.com.np/api/graphql";
-String API_IOS_BASE_LOCAL_URL = "http://localhost:8000/api/graphql";
-String API_BASE_LOCAL_MOBILE_URL = "https://oi.tinker.com.np/api/graphql"; //"http://192.168.31.221:8000/api/graphql";
-String OFFICE_LIST_URL = "https://ag.gov.np/dag1/offices.json";
-String FAQ_LIST_URL = "https://ag.gov.np/dag1/faqs.json";
+String OFFICE_LIST_URL = "";
+String FAQ_LIST_URL = "";
 String API_HIB_URL = 'http://imistest.hib.gov.np/api/graphql';
 
 String getBaseUrl(){
